bfce0cb7334ef8ae99a481ac30f25ab2633fc193
dannymasoft <danny@masoft.net>
2016-11-21 17:33:26 -0500

    comit _param

--- Presentacion/Php/Clases/ParametrosRpt.cs
+++ Presentacion/Php/Clases/ParametrosRpt.cs
@@ -7,8 +7,14 @@ namespace Presentacion.Php.Clases
 {
     public class ParametrosRpt
     {
+        //se agrega los parametros q se utilizaran en los reportes
+
         public string fecha_desde { get; set; }
         public string Fecha_hasta { get; set; }
         public string id_entidades { get; set; }
+        public string tipo_comprobantes { get; set; }
+        public string numero_comprobantes { get; set; }
+        public string referencia_doc_comprobantes { get; set; }
+
     }
 }
